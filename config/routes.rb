fef2673d55d9023dadc2caedd68f399a12472fee
Henry Dotson <henry.d.dotson@gmail.com>
2015-07-03 23:09:35 -0400

    set up devise and user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   
   root 'welcome#index'
+  devise_for :users
+  get ':username'=> 'internal#index'
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
